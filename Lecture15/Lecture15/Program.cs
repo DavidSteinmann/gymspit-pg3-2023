1ae130ceba32370a770657afb4cf803ea7a64c52
David Steinmann <davidsteinmann42@gmail.com>
2024-02-01 09:45:38 +0100

    Added Lecture15

---
+++ Lecture15/Lecture15/Program.cs
@@ -0,0 +1,103 @@
+﻿
+Cat garfield = new Cat("Garfield");
+Dog bernard = new Dog("Bernard");
+Animal john = new Animal("John", "duck");
+
+garfield.Speak();
+Animal garf = garfield;
+garf.Speak();
+
+garfield.SayHello();
+garfield.Speak();
+
+bernard.SayHello();
+bernard.Speak();
+bernard.Fetch();
+
+john.SayHello();
+john.Speak();
+
+class Animal
+{
+    public string Name;
+    public string Type;
+
+    public Animal(string name, string type)
+    {
+        Name = name;
+        Type = type;
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, I'm {Name}, I'm a {Type}.");
+    }
+
+    public virtual void Speak()
+    {
+        Console.WriteLine("Default speaking");
+        Console.WriteLine("I don't know");
+    }
+}
+
+class Dog : Animal
+{
+    public Dog(string name) : base(name, "dog")
+    {
+    }
+
+    public void Speak()
+    {
+        Console.WriteLine("dog overriden speaking");
+        Console.WriteLine("Bark");
+    }
+
+    public void Fetch()
+    {
+        Console.WriteLine("Fetching");
+    }
+}
+
+class Bulldog : Dog
+{
+    public Bulldog(string name) : base(name)
+    {
+    }
+}
+
+class Cat : Animal
+{
+    public Cat(string name) : base(name, "cat")
+    {
+    }
+
+    public override void Speak()
+    {
+        Console.WriteLine("cat overriden speaking");
+        Console.WriteLine("Meow");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
